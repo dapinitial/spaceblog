f3432170630195ffb5d4bd28b60325d0783dc4aa
David Puerto <me@davidpuerto.com>
2020-01-16 15:16:48 -0800

    Signin component and cookie/localstorage/authenticate

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,11 +1,12 @@
 import fetch from 'isomorphic-fetch';
-import { API } from '../config';
- 
+import {API} from '../config';
+import cookie from 'js-cookie';
+
 export const signup = async user => {
     try {
         const response = await fetch(`${API}/signup`, {
             method: 'POST',
- 
+
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
@@ -17,3 +18,73 @@ export const signup = async user => {
         return console.log(err);
     }
 };
+
+export const signin = async user => {
+    try {
+        const response = await fetch(`${API}/signin`, {
+            method: 'POST',
+
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        return response.json();
+    } catch (err) {
+        return console.log(err);
+    }
+};
+
+// set cookie
+export const setCookie = (key, value) => {
+    if (process.browser) {
+        cookie.set(key, value, {expires: 1});
+    }
+};
+
+export const removeCookie = key => {
+    if (process.browser) {
+        cookie.remove(key, {expires: 1});
+    }
+};
+
+// get cookie
+export const getCookie = key => {
+    if (process.browser) {
+        cookie.get(key);
+    }
+};
+
+// localstorage
+export const setLocalStorage = (key, value) => {
+    if (process.browser) {
+        localstorage.setItem(key, JSON.stringify(value));
+    }
+};
+
+export const removeLocalStorage = key => {
+    if (process.browser) {
+        localstorage.removeItem(key);
+    }
+};
+
+// authenticate user by passing data to cookie and localstorage
+export const authenticate = (data, next) => {
+    setCookie('token', data.token);
+    setLocalStorage('user', data.user);
+    next();
+};
+
+export const isAuth = () => {
+    if (process.browser) {
+        const cookieChecked = getCookie('token');
+        if (cookieChecked) {
+            if (localstorage.getItem('user')) {
+                return JSON.parse();
+            } else {
+                return false;
+            }
+        }
+    }
+}
--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { signup } from '../../actions/auth';
-import Router from 'next/router';
+import {useState} from 'react';
+import {signup} from '../../actions/auth';
 
 const SignupComponent = () => {
 
-    const [values, setValues] = useState({
+    const [values,
+        setValues] = useState({
         name: 'David',
         email: 'me@davidpuerto.com',
         password: 'testing123',
@@ -14,58 +14,106 @@ const SignupComponent = () => {
         showForm: true
     });
 
-    const { name, email, password, error, loading, message, showForm } = values;
-
-    const handleChange = name => e => {
-        e.preventDefault();
-        setValues({...values, error: false, [name]: e.target.value});
-        console.log(e.target.value);
-    };
+    const {
+        name,
+        email,
+        password,
+        error,
+        loading,
+        message,
+        showForm
+    } = values;
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.table({ name, email, password, error, loading, message, showForm });
-        setValues({ ...values, loading: true, error: false });
-        const user = { name, email, password };
+        /*console.table({
+            name,
+            email,
+            password,
+            error,
+            loading,
+            message,
+            showForm
+        });*/
+        setValues({
+            ...values,
+            loading: true,
+            error: false
+        });
+        const user = {
+            name,
+            email,
+            password
+        };
 
-        signup(user)
-        .then(data => {
+        signup(user).then(data => {
             if (data.error) {
-                setValues({ 
-                    ...values, 
-                    error: data.error, 
-                    loading: false 
+                setValues({
+                    ...values,
+                    error: data.error,
+                    loading: false
                 });
             } else {
                 setValues({
-                    ...values, 
-                    name: '', 
-                    email: '', 
-                    password: '', 
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
                     error: '',
-                    loading: false, 
-                    message: data.message, 
+                    loading: false,
+                    message: data.message,
                     showForm: false
                 });
             }
         });
     };
 
-    const showLoading = () => (loading ? <div className="alert alert-info">Loading...</div> : '');
-    const showError = () => (error ? <div className="alert alert-danger">{error}</div> : '');
-    const showMessage = () => (message ? <div className="alert alert-info">{message}</div> : '');
+    const handleChange = name => e => {
+        e.preventDefault();
+        setValues({
+            ...values,
+            error: false,
+            [name]: e.target.value
+        });
+        //console.log(e.target.value);
+    };
+
+    const showLoading = () => (loading
+        ? <div className="alert alert-info">Loading...</div>
+        : '');
+    const showError = () => (error
+        ? <div className="alert alert-danger">{error}</div>
+        : '');
+    const showMessage = () => (message
+        ? <div className="alert alert-info">{message}</div>
+        : '');
 
     const signupForm = () => {
         return (
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Type your name" />
+                    <input
+                        value={name}
+                        onChange={handleChange('name')}
+                        type="text"
+                        className="form-control"
+                        placeholder="Type your name"/>
                 </div>
                 <div className="form-group">
-                    <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Type your email" />
+                    <input
+                        value={email}
+                        onChange={handleChange('email')}
+                        type="email"
+                        className="form-control"
+                        placeholder="Type your email"/>
                 </div>
                 <div className="form-group">
-                    <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Type your password" />
+                    <input
+                        value={password}
+                        onChange={handleChange('password')}
+                        type="password"
+                        className="form-control"
+                        placeholder="Type your password"/>
                 </div>
                 <div>
                     <button className="btn btn-primary">Signup</button>
