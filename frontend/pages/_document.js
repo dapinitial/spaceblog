47dad8c902170eafa5f6aa98e580b145a17224bd
David Puerto <me@davidpuerto.com>
2020-01-21 13:56:59 -0800

    category-tag page

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,33 +1,37 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    return {
+      ...initialProps
+    }
   }
 
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="author" content="David Puerto" />
+          <meta charSet="UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta name="author" content="David Puerto"/>
 
-          <link 
-            rel="shortcut icon" 
+          <link
+            rel="shortcut icon"
             href="http://davidpuerto.com/favicon.ico"
-            type="image/ico" />
-            
-          <link 
-            rel="stylesheet" 
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
-            crossorigin="anonymous" />
+            type="image/ico"/>
+
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossorigin="anonymous"/>
+
+          <link rel="stylesheet" href="/static/css/styles.css"/>
         </Head>
         <body>
-          <Main />
-          <NextScript />
+          <Main/>
+          <NextScript/>
         </body>
       </Html>
     )
--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,9 +1,33 @@
 import Layout from '../../components/Layout';
+import Admin from '../../components/auth/Admin';
+import Link from 'next/link';
 
 const AdminIndex = () => {
   return (
     <Layout>
-      <h2>Admin Dashboard</h2>
+      <Admin>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>Admin Dashboard</h2>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link href="/admin/crud/category-tag">
+                    <a>Create Category</a>
+                  </Link>
+                </li>
+                <li className="list-group-item">ghgrhfgdh</li>
+                <li className="list-group-item">tertret</li>
+              </ul>
+            </div>
+            <div className="col-md-8">
+              Right
+            </div>
+          </div>
+        </div>
+      </Admin>
     </Layout>
   )
 };
